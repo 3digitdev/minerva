60babf2f6f264d463265c5dcf23c5c06dfca2f25
Max Bussiere <max@3digit.dev>
2020-06-29 17:41:47 -0500

    more skeleton work

---
+++ minerva/categories/notes.py
@@ -0,0 +1,11 @@
+import attr
+
+
+@attr.s
+class Note:
+    id: int = attr.ib()
+    content: str = attr.ib()
+    url: str = attr.ib(default="")
+
+    def __dict__(self):
+        return {'id': self.id, 'content': self.content, 'url': self.url}
---
+++ minerva/server.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+
+URL_BASE = '/api/v1'
+
+app = Flask(__name__)
+
+
+@app.route(f"{URL_BASE}/notes", methods=['GET', 'POST'])
+def all_notes():
+    if request.method == 'GET':
+        return 'TODO: GET ALL NOTES'
+    elif request.method == 'POST':
+        return 'TODO: CREATE NOTE'
+
+
+@app.route(f"{URL_BASE}/notes/<int:note_id>", methods=['GET', 'PUT'])
+def note_by_id(note_id: int):
+    if request.method == 'GET':
+        return 'TODO: GET NOTE BY ID'
+    elif request.method == 'PUT':
+        return 'TODO: UPDATE NOTE'
