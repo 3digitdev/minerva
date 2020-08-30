9a2d64665654a7c4d7c1716947b07a3691d0bcd6
Max Bussiere <max@3digit.dev>
2020-08-30 09:12:46 -0500

    create tags on demand

--- minerva/helpers/validators.py
+++ minerva/helpers/validators.py
@@ -6,7 +6,7 @@ from .exceptions import BadRequestError
 def validate_tag_list(instance, attr, value) -> None:
     """
     This is a helper function for making sure that when you add a Tag to a Category
-    record, there is a matching Tag object in the datastore already.  Also performs
+    record, a matching Tag object is created in the datastore.  Also performs
     basic type validation.  This is a method as defined by `attr` for "Validators".
     :param instance: IGNORED
     :param attr: IGNORED
@@ -18,9 +18,7 @@ def validate_tag_list(instance, attr, value) -> None:
             all_tags = [tag.name for tag in db.find_all_no_limit() if isinstance(tag, Tag)]
             for tag in value:
                 if tag not in all_tags:
-                    raise BadRequestError(
-                        f"Could not find a tag named [{tag}].  Please create the tag first."
-                    )
+                    db.create(Tag.from_request({"name": tag}))
     else:
         raise BadRequestError(f"Expected a list of strings but got [{value.__class__.__name__}]")
 
